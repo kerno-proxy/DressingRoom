0d60bc7dbb07924f685cdd3ccc5b792db73570ff
kerno-proxy <igorspirinv@gmail.com>
2021-05-31 14:05:29 +0200

    Animated mouse over on "start game". wow.

---
+++ Assets/Scripts/StartGameButtonScripts.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButtonScripts : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem particleSystem;
+    public void MouseOverEffects()
+    {
+        if (!particleSystem.isPlaying)
+        {
+            particleSystem.Play(); 
+        }
+    }
+}
